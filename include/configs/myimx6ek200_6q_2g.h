2c249cc7f63dc5d4cd8931924f4cc39e9284957d
Varphone Wong <varphone@qq.com>
2016-11-11 09:04:02 +0800

    Import MYZR u-boot-2009.8-svn56

---
+++ include/configs/myimx6ek200_6q_2g.h
@@ -0,0 +1,15 @@
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include <asm/arch/mx6.h>
+
+ /* High Level Configuration Options */
+#define CONFIG_MX6Q
+#define CONFIG_BOARD_IS_MYIMXEK200
+#define CONFIG_RAM_SIZE_IS_2G
+#define MYIMX6EK_SPEC	"6q-2g"
+
+#include "myimx6_common.h"
+
+#endif				/* __CONFIG_H */
