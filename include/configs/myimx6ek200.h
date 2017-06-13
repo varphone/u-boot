3d642b192370a40f4a43e5a85bb0f8249aff1791
黄华锋 <varphone@qq.com>
2017-06-13 16:19:25 +0800

    imx: myimx6ek200: update mmcroot env
    Signed-off-by: 黄华锋 <varphone@qq.com>

--- include/configs/myimx6ek200.h
+++ include/configs/myimx6ek200.h
@@ -17,7 +17,7 @@
 
 #define CONFIG_MXC_UART_BASE		UART1_BASE
 #define CONFIG_CONSOLE_DEV		"ttymxc0"
-#define CONFIG_MMCROOT			"/dev/mmcblk2p2"  /* SDHC3 */
+#define CONFIG_MMCROOT			"/dev/mmcblk3p2 rootfstype=squashfs overlayroot=/dev/mmcblk3p4:rw:ext4"  /* eMMC */
 
 #if defined(CONFIG_MX6QP)
 #define PHYS_SDRAM_SIZE			(1u * 1024 * 1024 * 1024)
